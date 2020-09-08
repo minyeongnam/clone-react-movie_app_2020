2042301e72ea341a0af8437948fea6fb3ef172dc
MINYEONGNAM <min@MacBook-Pro.local>
2020-09-09 01:43:13 +0900

    2days

--- src/App.js
+++ src/App.js
@@ -1,56 +1,24 @@
 import React from 'react';
 
-function Food({ name, picture }){
-	return (
-		<div>
-			<h2>I like {name}</h2>
-			<img src={picture} alt="" />
-		</div>
-	);	
-}
-
-const foodLike = [
-	{
-		id:1,
-		name: 'kimchi',
-		image: 'https://shopping-phinf.pstatic.net/main_1065829/10658291585.20191231172531.jpg?type=f300'
-	},
-	{
-		id:2,
-		name: 'banana',
-		image: 'https://search.pstatic.net/common/?src=http%3A%2F%2Fpost.phinf.naver.net%2FMjAxNzExMDhfMTQ3%2FMDAxNTEwMTA1NDA2ODk4.TRPE9rtezG3xTDfLo53VzXf057Jpwn5cM3UOkbEqxuwg.sbvqhbGklBnY9JQoExej7aveJbByfLovYq5MMO2Pbs0g.JPEG%2Fmug_obj_151010531834351974.jpg&type=sc960_832'
-	},
-	{
-		id:3,
-		name: 'gagaga',
-		image: 'https://shopping-phinf.pstatic.net/main_1065829/10658291585.20191231172531.jpg?type=f300'
-	},
-	{
-		id:4,
-		name: 'nanana',
-		image: 'https://search.pstatic.net/common/?src=http%3A%2F%2Fpost.phinf.naver.net%2FMjAxNzExMDhfMTQ3%2FMDAxNTEwMTA1NDA2ODk4.TRPE9rtezG3xTDfLo53VzXf057Jpwn5cM3UOkbEqxuwg.sbvqhbGklBnY9JQoExej7aveJbByfLovYq5MMO2Pbs0g.JPEG%2Fmug_obj_151010531834351974.jpg&type=sc960_832'
-	},
-	{
-		id:5,
-		name: 'dadada',
-		image: 'https://shopping-phinf.pstatic.net/main_1065829/10658291585.20191231172531.jpg?type=f300'
-	},
-	{
-		id:6,
-		name: 'fafafa',
-		image: 'https://search.pstatic.net/common/?src=http%3A%2F%2Fpost.phinf.naver.net%2FMjAxNzExMDhfMTQ3%2FMDAxNTEwMTA1NDA2ODk4.TRPE9rtezG3xTDfLo53VzXf057Jpwn5cM3UOkbEqxuwg.sbvqhbGklBnY9JQoExej7aveJbByfLovYq5MMO2Pbs0g.JPEG%2Fmug_obj_151010531834351974.jpg&type=sc960_832'
+class App extends React.Component{
+	state = {
+		count:0,
+	};
+	add = () => {
+		this.setState(current => ({count: current.count+1}));
 	}
-];
-
-function App() {
-	return (
-		<div>
-			<h1>hello</h1>
-			{foodLike.map(dish => {
-				return <Food name={dish.name} picture={dish.image} key={dish.id} />;
-			})}
-		</div>
-  	);
+	del = () => {
+		this.setState(current => ({count: current.count -1}));
+	}
+	render() {
+		return (
+			<>
+				<h1>the number is: {this.state.count}</h1>
+				<button type="button" onClick={this.add}>add</button>
+				<button type="button" onClick={this.del}>del</button>
+			</>
+		);
+	}
 }
 
 export default App;
