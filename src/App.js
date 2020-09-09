77971209d045710c55a85164493f18c9b272fd7b
MINYEONGNAM <min@MacBook-Pro.local>
2020-09-10 00:17:53 +0900

    3days

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
 import React from 'react';
+import axiso from 'axios';
+import Movie from './Movie';
 
 class App extends React.Component{
 	state= {
 		isLoading:true,
 		movies:[],
-	}
-	componentDidMount(){
-		setTimeout(()=>{
-			this.setState({isLoading:false});
-		}, 6000);
+	};	
+	componentDidMount(){	
 		//영화데이터로딩
+		this.getMovies();
+	}
+	getMovies = async() => {
+		const {
+			data: {
+				data: {movies},
+			},
+		} = await axiso.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+		this.setState({movies, isLoading:false});
 	}
 	render() {
-		const {isLoading} = this.state;
-		return <div>{isLoading ? "Loding...	" : "we are ready"}</div>;
+		const {isLoading, movies} = this.state;
+		return (
+			<div>
+				{isLoading ? "Loding...	" : movies.map((item) => {
+					console.log(movies)
+					return (
+						<Movie 
+							id={item.id}
+							year={item.year}
+							title={item.title}
+							summary={item.summary}
+							title={item.poster}
+						/>
+					);
+				})}
+			</div>
+		);
 	}
 }
 
